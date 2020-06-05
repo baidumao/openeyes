1c1643df47862fcce86834b0465e84e00bdc9edf
maosy <Founder@2011>
2020-06-05 13:59:25 +0800

    启动白屏修改

--- android/app/src/main/kotlin/com/flutteropenyes/MainActivity.kt
+++ android/app/src/main/kotlin/com/flutteropenyes/MainActivity.kt
@@ -1,6 +1,13 @@
 package com.flutteropenyes
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import io.flutter.embedding.android.FlutterActivity
+import org.devio.flutter.splashscreen.SplashScreen
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        SplashScreen.show(this, true)
+        super.onCreate(savedInstanceState, persistentState)
+    }
 }
